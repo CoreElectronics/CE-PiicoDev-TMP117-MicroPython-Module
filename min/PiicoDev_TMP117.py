b1276b2be59dbb18d1d95874d9ba3d99787bf52b
GitHub Action <action@github.com>
2021-09-24 00:01:31 +0000

    Update min/PiicoDev_TMP117.py

--- min/PiicoDev_TMP117.py
+++ min/PiicoDev_TMP117.py
@@ -1,6 +1,6 @@
 _A=None
 from PiicoDev_Unified import *
-REG_TEMPC=b'\x00'
+REG_TEMPC=0
 compat_str='\nUnified PiicoDev library out of date.  Get the latest module: https://piico.dev/unified \n'
 class PiicoDev_TMP117:
 	def __init__(self,bus=_A,freq=_A,sda=_A,scl=_A,addr=72):
@@ -10,7 +10,7 @@ class PiicoDev_TMP117:
 		except:print(compat_str)
 		self.i2c=create_unified_i2c(bus=bus,freq=freq,sda=sda,scl=scl);self.addr=addr
 	def readTempC(self):
-		try:data=self.i2c.read16(self.addr,REG_TEMPC)
+		try:data=self.i2c.readfrom_mem(self.addr,REG_TEMPC,2)
 		except:print(i2c_err_str.format(self.addr));return float('NaN')
 		tempDataRaw=int.from_bytes(data,'big')
 		if tempDataRaw>=32768:return-256.0+(tempDataRaw-32768)*0.0078125
