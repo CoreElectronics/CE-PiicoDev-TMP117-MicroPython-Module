ee0b9f41f81adbae3a376388b739f2f829260fa7
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-10-22 09:40:58 +1100

    use PiicoDev_Unified.sleep_ms()

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 # and displays the result in Degrees Celsius, Farenheit and Kelvin
 
 from PiicoDev_TMP117 import PiicoDev_TMP117
-from time import sleep
+from PiicoDev_Unified import sleep_ms # cross-platform compatible sleep function
 
-tempSensor = PiicoDev_TMP117()
+tempSensor = PiicoDev_TMP117() # initialise the sensor
 
 while True:
     # Read and print the temperature in various units
@@ -16,4 +16,4 @@ while True:
     # Convert temperature into a string and print the data
     print(str(tempC) + " °C")
     
-    sleep(1) # delay
+    sleep_ms(1000) # delay 1 second
